5550c30d3a9e8ecb78aa53eadcaaf6f0cc58c5a3
윤지 <85012406+yoonhihi97@users.noreply.github.com>
2025-12-04 20:42:56 +0900

    chore: GitHub Pages 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: '/front_7th_chapter3-2/',
+    build: {
+      outDir: 'docs',
+      rollupOptions: {
+        input: './index.advanced.html',
+      },
+    },
   }),
   defineTestConfig({
     test: {
